f22c8f0ee27d7b2d12233a2779970ae49101eabf
Ola Ajibode <ola.ajibode@gmail.com>
2022-11-28 00:19:37 +0000

    Feat: Add sample todos

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success}
 
 object Main extends App {
   val host = "0.0.0.0"
-  val port = 9000
+  val port = 9001
 
   implicit val system: ActorSystem = ActorSystem(name = "todoapi")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
@@ -14,7 +14,12 @@ object Main extends App {
   import akka.http.scaladsl.server.Directives._
   import system.dispatcher
 
-  val todoRepository = new InMemoryTodoRepository()
+  val todoRepository = new InMemoryTodoRepository(Seq(
+    Todo("1", "Cook plain eggs", "Water-based egg meals", false),
+    Todo("2", "Cook oily eggs", "Oil-based egg meals", true),
+    Todo("3", "Shell cooked eggs", "Just cook the shells as meals", false),
+  ))
+
   val router = new TodoRouter(todoRepository)
   val server = new Server(router, host, port)
   val binding = server.bind()
